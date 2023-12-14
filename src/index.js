ff2dff77d4fe970902820647c4ee8b614ab4a8ec
Peri2505 <moldobaevaperi@yahoo.com>
2023-12-14 14:17:39 -0600

    #

--- src/index.js
+++ src/index.js
@@ -57,11 +57,6 @@ function App() {
   );
 }
 function Header() {
-  //   const style = {
-  //     color: "magenta",
-  //     fontSize: "36px",
-  //     textTransform: "uppercase",
-  //   };
   const style = {};
   return (
     <header className="header">
@@ -71,45 +66,40 @@ function Header() {
 }
 function Menu() {
   const pizzas = pizzaData;
+  //const pizzas = [];
   const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We are still working on our menu. Please com eback later</p>
       )}
-
-      {/* <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
-        photoName="pizzas/spinaci.jpg"
-        price={10}
-      />
-      <Pizza
-        name="Pizza Fungi"
-        ingredients="Tomato,mushrooms"
-        price={12}
-        photoName="pizzas/funghi.jpg"
-      /> */}
     </main>
   );
 }
-function Pizza(props) {
-  if (props.pizzaObj.soldOut) return null;
+function Pizza({ pizzaObj }) {
+  //if (pizzaObj.soldOut) return null;
   return (
-    <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -121,8 +111,7 @@ function Footer() {
   const openHour = 8;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  //   if (hour >= openHour && hour <= closeHour) alert("We are currently open");
-  //   else alert("Sorry we are closed");
+
   if (!isOpen)
     return (
       <p>
@@ -132,18 +121,26 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>
-            We are open until {closeHour}:00. Come visit us or order online.
-          </p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We are happy to welcome you between{openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
+  );
+}
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <div className="order">
+        <p>
+          We are openfrom {openHour}:00 to {closeHour}:00. Come visit us or
+          order online.
+        </p>
+        <button className="btn">Order</button>
+      </div>
+    </div>
   );
 }
 
